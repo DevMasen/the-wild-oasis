d5c9a97e0aa125d9ce17665292f9ae880e39062b
DevMasen <mohammadwh400@gmail.com>
2026-02-26 22:33:53 +0330

    feat: add some styled components to app

--- README.md
+++ README.md
@@ -6,3 +6,35 @@ Currently, two official plugins are available:
 
 - [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
 - [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+
+---
+
+# Get Started with Styled Components :
+
+installation: `npm i styled-components`
+
+`App.jsx`:
+
+```js
+import styled from 'styled-components';
+
+const Element = styled.element`
+	// CSS styles
+	margin: 30px;
+	padding: 20px;
+`;
+
+const StyledApp = styled.div`
+	// Styles for App Component
+	padding: 30px;
+	background-color: black;
+`;
+
+export default function App() {
+	return (
+		<StyledApp>
+			<Element>Foo Baz</Element>
+		</StyledApp>
+	);
+}
+```
--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,50 @@
+import styled from 'styled-components';
+
+const H1 = styled.h1`
+	font-size: 72px;
+	font-weight: 600;
+	color: #777;
+`;
+
+const Button = styled.button`
+	margin: 3rem;
+	padding: 2rem 5rem;
+	border: none;
+	outline: none;
+	background-color: purple;
+	color: red;
+	border-radius: 5px;
+	cursor: pointer;
+`;
+
+const Input = styled.input`
+	border: none;
+	outline: none;
+	padding: 1rem;
+	margin: 2rem;
+	border: 1px solid #ddd;
+	border-radius: 5px;
+`;
+
+const StyledApp = styled.div`
+	padding: 1rem;
+	height: 100dvh;
+	background-color: black;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+`;
+
 function App() {
-	return <div>Hello, Vite</div>;
+	return (
+		<StyledApp>
+			<H1>Hello, Masein</H1>
+			<Button onClick={() => alert('Checked In')}>Check in</Button>
+			<Button onClick={() => alert('Checked Out')}>Check out</Button>
+			<Input defaultValue={26} type="number" />
+		</StyledApp>
+	);
 }
 
 export default App;
