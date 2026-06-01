36527d6ac3dff8c1e3f8d2cd9e939ad756086191
DevMasen <mohammadwh400@gmail.com>
2026-06-01 20:24:10 +0330

    chore: prettier edit

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,9 +1,7 @@
 import { Outlet } from 'react-router-dom';
-///////////////////////////////////////////
 import Sidebar from './Sidebar';
 import Header from './Header';
 import styled from 'styled-components';
-///////////////////////////////
 
 const StyledAppLayout = styled.div`
 	height: 100dvh;
