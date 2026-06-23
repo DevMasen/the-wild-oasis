b0744424afe6ef3ab86c5ea4ba5703debd47725c
DevMasen <mohammadwh400@gmail.com>
2026-06-23 05:37:07 +0330

    E405: feat: project completed!

--- src/context/DarkModeContext.jsx
+++ src/context/DarkModeContext.jsx
@@ -7,7 +7,7 @@ const DarkModeContext = createContext();
 
 function DarkModeProvider({ children }) {
 	const [isDarkMode, setIsDarkMode] = useLocalStorageState(
-		false,
+		window.matchMedia('(prefer-color-scheme: dark)').matches,
 		'isDarkMode',
 	);
 
--- src/features/bookings/BookingDetail.jsx
+++ src/features/bookings/BookingDetail.jsx
@@ -18,6 +18,7 @@ import ButtonText from '../../ui/ButtonText';
 import Spinner from '../../ui/Spinner';
 import Modal from '../../ui/Modal';
 import ConfirmDelete from '../../ui/ConfirmDelete';
+import Empty from '../../ui/Empty';
 //---
 
 const HeadingGroup = styled.div`
@@ -29,12 +30,10 @@ const HeadingGroup = styled.div`
 function BookingDetail() {
 	const navigate = useNavigate();
 
-	const { booking = {}, isLoading } = useBooking();
+	const { booking, isLoading } = useBooking();
 	const moveBack = useMoveBack();
 	const { checkout, isCheckingOut } = useCheckout();
 	const { deleteBooking, isDeleting } = useDeleteBooking();
-
-	const { status, id: bookingId } = booking;
 
 	const statusToTagName = {
 		unconfirmed: 'blue',
@@ -43,6 +42,10 @@ function BookingDetail() {
 	};
 
 	if (isLoading) return <Spinner />;
+
+	if (!booking) return <Empty resourceName="booking" />;
+
+	const { status, id: bookingId } = booking;
 
 	return (
 		<>
